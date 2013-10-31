2a7a98d2ed69bbb61352aa7e82901e5e2c591a55
Bruno Binet <bruno.binet@gmail.com>
2013-10-31 19:05:09 +0100

    Make cfgpath an attribute

--- test_repostack.py
+++ test_repostack.py
@@ -11,18 +11,18 @@ class TestRepoStack(unittest.TestCase):
     def setUp(self):
         self.tmpdir = tempfile.mkdtemp(prefix='test_repostack_')
         self.rs = RepoStack(rootdir=self.tmpdir)
+        self.cfgpath = os.path.join(self.tmpdir, '.repostack')
 
     def tearDown(self):
         shutil.rmtree(self.tmpdir)
 
     def test_init(self):
-        cfgpath = os.path.join(self.tmpdir, '.repostack')
-        self.assertEqual(cfgpath, self.rs.cfg_abspath)
-        self.assertFalse(os.path.exists(cfgpath))
+        self.assertEqual(self.cfgpath, self.rs.cfg_abspath)
+        self.assertFalse(os.path.exists(self.cfgpath))
 
         # init ok if not already managed by repostack
         self.rs.init({})
-        self.assertTrue(os.path.exists(cfgpath))
+        self.assertTrue(os.path.exists(self.cfgpath))
 
         # init ko if already managed by repostack
         with self.assertRaises(Exception):
@@ -39,7 +39,7 @@ class TestRepoStack(unittest.TestCase):
         self.assertFalse(os.path.exists(child_tmpdir))
 
         # init ok if parent dirs not already managed by repostack
-        os.remove(cfgpath)
+        os.remove(self.cfgpath)
         child_rs.init({})
         self.assertTrue(os.path.exists(child_tmpdir))
         self.assertTrue(os.path.exists(child_cfgpath))
