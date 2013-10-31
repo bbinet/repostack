5e0f58ed2ee14347553aa6ce891be9bb2a20bee9
Bruno Binet <bruno.binet@gmail.com>
2013-10-31 14:52:14 +0100

    Improve test for init command

--- test_repostack.py
+++ test_repostack.py
@@ -19,10 +19,30 @@ class TestRepoStack(unittest.TestCase):
         cfgpath = os.path.join(self.tmpdir, '.repostack')
         self.assertEqual(cfgpath, self.rs.cfg_abspath)
         self.assertFalse(os.path.exists(cfgpath))
+
+        # init ok if not already managed by repostack
         self.rs.init({})
         self.assertTrue(os.path.exists(cfgpath))
+
+        # init ko if already managed by repostack
         with self.assertRaises(Exception):
             self.rs.init({})
+
+        # init ko if a parent dir is already managed by repostack
+        child_tmpdir = os.path.join(self.tmpdir, 'child', 'tmpdir')
+        self.assertFalse(os.path.exists(child_tmpdir))
+        child_cfgpath = os.path.join(child_tmpdir, '.repostack')
+        child_rs = RepoStack(rootdir=child_tmpdir)
+        self.assertEqual(child_cfgpath, child_rs.cfg_abspath)
+        with self.assertRaises(Exception):
+            child_rs.init({})
+        self.assertFalse(os.path.exists(child_tmpdir))
+
+        # init ok if parent dirs not already managed by repostack
+        os.remove(cfgpath)
+        child_rs.init({})
+        self.assertTrue(os.path.exists(child_tmpdir))
+        self.assertTrue(os.path.exists(child_cfgpath))
 
 
 if __name__ == '__main__':
